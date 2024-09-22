e2fff61f0d0db176c7c509003c559975aac2e096
ri-gusev <gusev.roman1@mail.ru>
2024-09-22 22:43:29 +0300

    |

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,75 +1,21 @@
+
 public class Main {
-
-    private static final Object MONITOR = new Object();
-    private static final String A = "A";
-    private static final String B = "B";
-    private static final String C = "C";
-    private static String nextLetter = A;
-
     public static void main(String[] args) {
 
         new Thread(new Runnable() {
             @Override
-            public void run(){
-                synchronized (MONITOR){
-                    for (int i = 0; i < 5; i++) {
-                        try {
-                            while (!nextLetter.equals(A)){
-                                MONITOR.wait();
-                            }
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        System.out.print(A);
-                        nextLetter = B;
-                        MONITOR.notifyAll();
-                    }
-                }
+            public void run() {
+                Printer.print(15);
+                Printer.print(1);
             }
         }).start();
 
         new Thread(new Runnable() {
             @Override
-            public void run(){
-                synchronized (MONITOR){
-                    for (int i = 0; i < 5; i++) {
-                        try {
-                            while (!nextLetter.equals(B)){
-                                MONITOR.wait();
-                            }
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        System.out.print(B);
-                        nextLetter = C;
-                        MONITOR.notifyAll();
-                    }
-                }
+            public void run() {
+                Printer.scan(15);
+                Printer.scan(1);
             }
         }).start();
-
-        new Thread(new Runnable() {
-            @Override
-            public void run(){
-                synchronized (MONITOR){
-                    for (int i = 0; i < 5; i++) {
-                        try {
-                            while (!nextLetter.equals(C)){
-                                MONITOR.wait();
-                            }
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        System.out.print(C);
-                        nextLetter = A;
-                        MONITOR.notifyAll();
-                    }
-                }
-            }
-        }).start();
-
-        // expected output - ABCABCABCABCABC   (correct)
-
-        
     }
 }
