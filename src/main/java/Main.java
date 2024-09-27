dac546d661d380e85266af445e21b0e40428029e
ri-gusev <gusev.roman1@mail.ru>
2024-09-27 17:14:37 +0300

    CopyOnWriteArrayList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
     public static void main(String[] args) {
 
+        List<Integer> array = new CopyOnWriteArrayList<>();
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        Object obj = new Object();
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Printer.print(15);
-                Printer.print(1);
+                try {
+                    for (int i = 0; i < 100; i++) {
+                        Thread.sleep(100);
+                        array.add(i);
+                    }
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }).start();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Printer.scan(15);
-                Printer.scan(1);
+                try {
+                    for (int i = 0; i < 100; i++) {
+                        Thread.sleep(100);
+                        array.add(i);
+                    }
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }).start();
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(array.size());
+
+        for (int x : array){
+            System.out.println(x);
+        }
+
     }
 }
